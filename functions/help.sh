79adcd50bc82c99bb2c10facc597d619ed4a9e78
David du Toit <dhdutoit@gmail.com>
2018-10-24 08:29:23 +0200

    Generelized the printing of help function details and added use of colour in the command output * updated help functions for the commands: PUSH, PULL, MERGE and COMMIT

--- functions/help.sh
+++ functions/help.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+function printHelp {
+	local command="$1"
+  	local usage="$2"
+	shift
+	shift
+	descriptions=("$@")
+
+	echo
+	printf "%s\n" "${cyan}${command}${end} : ${yellow}oc ${usage}${end}"
+
+	for i in "${descriptions[@]}"
+	do
+		printf "%s\t%s\n" "" "${green}${i}${end}"
+	done
+}
+
 function help {
 	for i in "${!GMT_CMD[@]}"
 	do
