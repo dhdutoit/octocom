908e24e38f7b598d3cc05db73760c9ed281cce2c
David du Toit <dhdutoit@gmail.com>
2020-04-19 15:35:19 +0200

    Syntax updates in PULL command

--- functions/git_pull.sh
+++ functions/git_pull.sh
@@ -6,18 +6,18 @@
 function pl {
 	remote="origin"
 
-	if [ $1 ]; then
+	if [ "$1" ]; then
 		#check if the provided remote actually exists
-		git ls-remote --exit-code $1 2>$1
+		git ls-remote --exit-code "$1" 2>/dev/null
 		if [ $? != 0 ] && [ $? != 2 ] ; then
-			echo remote: could not read from remote $remote, please check connection or access credentials and try again
+			echo remote: could not read from remote "$remote", please check connection or access credentials and try again
 			exit 1
 		fi
 
 		remote=$1
 	fi
 
-	git pull $remote "$(git name-rev --name-only HEAD)"
+	git pull "$remote" "$(git name-rev --name-only HEAD)"
 	exit 0
 }
 
