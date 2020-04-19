7ac19f08ca9038ef6186f352dac22247a37a8e7f
David du Toit <dhdutoit@gmail.com>
2020-04-19 15:22:07 +0200

    Update function to allow creation of new brach for updating of functions

--- functions/git_checkout.sh
+++ functions/git_checkout.sh
@@ -8,15 +8,15 @@ function co {
 	branch="master"
 	repo=""
 
-	if [ $1 ]; then
+	if [ "$1" ]; then
 		branch=$1
 	fi
 
-	if [ $2 ]; then
+	if [ "$2" ]; then
 		#check if the provided remote actually exists
-		git ls-remote --exit-code $2 2>/dev/null
+		git ls-remote --exit-code "$2" 2>/dev/null
 		if [ $? != 0 ] && [ $? != 2 ] ; then
-			echo remote: octocom could not read from remote $2, please double check everything is ok with the remote and try again..perhaps..?
+			echo remote: octocom could not read from remote "$2", please double check everything is ok with the remote and try again?
 			exit 1
 		fi
 
@@ -24,19 +24,20 @@ function co {
 	fi
 
 	#get repo url
-	repo=git config --get remote.$remote.url 2>/dev/null
+	repo=git config --get remote."$remote".url 2>/dev/null
 
 	#check if branch exists on REMOTE
-	git ls-remote --exit-code --heads $repo $1 2>/dev/null
+	git ls-remote --exit-code --heads $repo "$1" 2>/dev/null
 	if [ $? == 2 ]; then
-		echo branch: could not read from branch $1, please check connection to repo or that branch exists on remote
+		echo branch: could not read from branch "$1", please check connection to repo or that branch exists on remote
 		exit 1
 	else
 		#do a fetch in case the branch in new in remote but never checked out lcoally
-		ft $remote
+		echo Fetching "$remote" details of "$1"
+		ft "$remote"
 	fi
 
-	git checkout $branch
+	git checkout "$branch"
 	exit 0
 }
 
